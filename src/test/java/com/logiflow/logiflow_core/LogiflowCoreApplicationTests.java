eb9d4d14cf30cd45200ec5e37a152e7383ea1c3f
AlexApari <leo08.06.1998@gmail.com>
2025-11-27 08:37:12 -0500

    Primer  commit

---
+++ src/test/java/com/logiflow/logiflow_core/LogiflowCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logiflow.logiflow_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiflowCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
