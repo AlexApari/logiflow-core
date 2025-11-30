877f6eb9c4e85c5bb12b939eef904da469da33fe
AlexApari <leo08.06.1998@gmail.com>
2025-11-30 14:53:27 -0500

    Add DetallePedidoDTO class for order details

---
+++ src/main/java/com/logiflow/logiflow_core/dto/request/DetallePedidoDTO.java
@@ -0,0 +1,9 @@
+package com.logiflow.logiflow_core.dto.request;
+
+
+public class DetallePedidoDTO {
+  private Producto productoId;
+  private Integer cantidad;
+  private BigDecimal descuentoPorcentaje;
+  private String notas;
+}
